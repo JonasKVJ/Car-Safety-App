3695fce0604c9df394b5889a5541bafaba4825f2
Jonas Stagnaro <jonaskaneborg@gmail.com>
2018-08-20 13:39:09 -0700

    - Added a toast message in response to toast button onClick - Changed the button background color to gray - Set the text size of all three textViews to 16sp - Added a second activity called q1_feedback

--- app/src/main/java/org/carsafe/jonasvinterjensen/asfcertificate/MainActivity.java
+++ app/src/main/java/org/carsafe/jonasvinterjensen/asfcertificate/MainActivity.java
@@ -1,7 +1,11 @@
 package org.carsafe.jonasvinterjensen.asfcertificate;
 
+import android.graphics.Typeface;
+import android.support.v4.graphics.TypefaceCompatUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toastOnClick(View view)
+    {
+        //Toast myToast = Toast.makeText(this, message, duration);
+        Toast myToast = Toast.makeText(this, "Testing how toasts works",
+                Toast.LENGTH_SHORT);
+        myToast.show();
     }
 }
