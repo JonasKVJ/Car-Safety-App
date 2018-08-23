8936fe06b6e915285edecf134d3110cd69aca8cd
Jonas Stagnaro <jonaskaneborg@gmail.com>
2018-08-22 18:36:22 -0700

    Clicking the first answer not opens a new activity

--- app/src/main/java/org/carsafe/jonasvinterjensen/asfcertificate/MainActivity.java
+++ app/src/main/java/org/carsafe/jonasvinterjensen/asfcertificate/MainActivity.java
@@ -1,13 +1,17 @@
 package org.carsafe.jonasvinterjensen.asfcertificate;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v4.graphics.TypefaceCompatUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    public int choice = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +25,18 @@ public class MainActivity extends AppCompatActivity {
         Toast myToast = Toast.makeText(this, "Testing how toasts works",
                 Toast.LENGTH_SHORT);
         myToast.show();
+    }
+
+    public void answer1_OnClick(View view)
+    {
+        String selectedTextView = "";
+
+        //Create an Intent object, which is required for a new activity
+        Intent answerIntent = new Intent(this, Q1_Feedback.class);
+        startActivity(answerIntent);
+        TextView textview = (TextView) view;
+        selectedTextView = textview.getText().toString();
+        //This will work in an OnlickListener, using a RecyclerView
+        System.out.println("\tSelected textView: \t" + selectedTextView);
     }
 }
