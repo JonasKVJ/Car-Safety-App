2cc2d2acfbe353718172b141c9965596d7e6c6f2
Jonas Stagnaro <jonaskaneborg@gmail.com>
2018-09-01 18:43:46 -0700

    Added a new theme, and also background colors based on the correctness of an answer. This was the final update before recording the short video demonstration.

--- app/src/main/java/org/carsafe/jonasvinterjensen/asfcertificate/Q1_Feedback.java
+++ app/src/main/java/org/carsafe/jonasvinterjensen/asfcertificate/Q1_Feedback.java
@@ -1,24 +1,26 @@
 package org.carsafe.jonasvinterjensen.asfcertificate;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Q1_Feedback extends AppCompatActivity {
 
     protected int choice = 0;
+    protected boolean isAnswerCorrect = false;
     protected boolean timerRanOut = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_q1__feedback);
-
-
         Bundle bundle = getIntent().getExtras();
         try
         {
@@ -29,27 +31,65 @@ public class Q1_Feedback extends AppCompatActivity {
         {
             timerRanOut = true;
         }
+        if(choice==2)
+        {
+            setTheme(R.style.correctAnswerTheme);
+        }
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q1__feedback);
+
+
+
+
+        TextView feedbackTitle = findViewById(R.id.q1feedback_title);
         TextView choiceView = findViewById(R.id.Answer);
+        View root = choiceView.getRootView();
+
+
         //Feedback will be given based on the answer, iff the user answered before the timer ran out
         if(!timerRanOut)
         {
             if(choice == 1)
             {
                 choiceView.setText(R.string.answer1);
+                feedbackTitle.setText("Incorrect");
+                feedbackTitle.setTextColor(Color.parseColor("#000000"));
+                //set background to red, since the answer was wrong
+                root.setBackgroundColor(Color.parseColor("#e57979"));
             }
             else if(choice == 2)
             {
+                feedbackTitle.setText("Correct");
+                feedbackTitle.setTextColor(Color.parseColor("#000000"));
+
+                //Set Toolbar color to black, because the rest of the screen will be green
+                getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+                root.setBackgroundColor(Color.parseColor("#68c17b"));
+
+
                 choiceView.setText(R.string.answer2);
             }
             else if(choice == 3)
             {
+                feedbackTitle.setText("Incorrect");
+                feedbackTitle.setTextColor(Color.parseColor("#000000"));
+                //set background to red, since the answer was wrong
+                root.setBackgroundColor(Color.parseColor("#e57979"));
+
                 choiceView.setText(R.string.answer3);
             }
         }
+        //Timer ran out
         else
         {
             choiceView.setText(R.string.timeRanOut);
+            feedbackTitle.setTextColor(Color.parseColor("#000000"));
+
+            //Set Toolbar color to black, because the rest of the screen will be green
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+            root.setBackgroundColor(Color.parseColor("#e57979"));
+
         }
 
     }
